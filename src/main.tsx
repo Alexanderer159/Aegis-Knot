854dfa11206c394bcc501d9db40e427c797aa3ca
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2026-08-08 20:38:30 +0200

    Grainy

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const noiseUrl = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='128' height='128'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='2' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E")`;
+
+createRoot(document.getElementById("root")!).render(
+  <>
+    <div className="pointer-events-none fixed inset-0 z-[9999] opacity-[0.07]" style={{ backgroundImage: noiseUrl, backgroundRepeat: "repeat", backgroundSize: "128px 128px",}}/>
+    <App />
+  </>
+);
