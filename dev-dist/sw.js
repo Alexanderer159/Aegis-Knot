534fbc0e08083b39b825fcd53bf10f00984c164d
Alejandro De Yavorsky <aledeyavorsky@gmail.com>
2026-08-08 20:19:30 +0200

    BG look try 1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1q2fstv6lac"
+    "revision": "0.ar3l9e2up1s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
